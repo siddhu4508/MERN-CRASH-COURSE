b0dbe47aea5ebc0e448d789b6ae469cd91b08610
Siddartha Yadav Sangam Reddy <siddhuyadav508@gmail.com>
2025-02-22 07:36:03 +0530

    added product.route and modified server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -9,34 +10,10 @@ const app = express();
 
 app.use(express.json()); // allows us to accept json date in the req.body acts as middleware
 
-app.post('/api/products', async (req, res) => {
-    const product = req.body; // user will send this data
-    
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success:false, message: "Please provide all fields"});
-    }
 
-    const newProduct = new Product(product);
-    
-    try {
-        await newProduct.save();
-        res.status(200).json({ success:true, data: newProduct});
-    } catch (error) {
-        console.error("Error in Creating Product:", error.message);
-        res.status(500).json({ success:false, message: "Server Error"});
-    }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-    const {id} = req.params;
-    
-    try {
-        await Product.findByIdAndDelete
-    } catch (error) {
-        
-    }
-});
  
+
+
 
 app.listen(5000, () => {
     connectDB();
